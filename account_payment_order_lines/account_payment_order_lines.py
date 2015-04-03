858526bd777b851e56c38c2795c543b9bb1a4200
John Doe <fabian.semal@smartsolution.be>
2015-04-03 15:06:48 +0200

    [ADD] account_payment_order_lines: Initial commi

---
+++ account_payment_order_lines/account_payment_order_lines.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Smart Solution bvba
+#    Copyright (C) 2010-Today Smart Solution BVBA (<http://www.smartsolution.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################## 
+
+from osv import osv, fields
+from openerp.tools.translate import _
+
+class wizard_account_payment_order_split(osv.osv_memory):
+
+    _name = 'wizard.account.payment.order.split'
+
+    def porder_split(self, cr, uid, ids, context=None):
+        """split payment order"""
+        for porder in self.pool.get('payment.order').browse(cr, uid, context['active_ids']):
+            line_ids = [line.id for line in porder.line_ids]
+            line_nbr = len(line_ids)
+            print "LINE IDS:",line_ids
+            print "LINE NBR:",line_nbr
+            while line_nbr > 1000:
+                new_porder = self.pool.get('payment.order').copy(cr, uid, porder.id, context=context)
+                new_line_ids = line_ids[1000:]
+                line_ids = line_ids[:1000]
+                self.pool.get('payment.line').write(cr, uid, new_line_ids, {'order_id':new_porder})
+        return True
+
+#class payment.order.line(osv.osv):
+#
+#    _inherit = 'payment.order.line'
+#
+#    def invoices_pay(self, cr, uid, ids, context=None):
+#        for pol in self.browse(cr, uid, ids):
+#            continue
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
